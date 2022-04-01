03057f1bc2916b9d61f1ea15d71caa998954ab44
PHub22f <102835794+PHub22f@users.noreply.github.com>
2022-04-02 01:07:42 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,68 @@
+let video = document.querySelector('.Video');
+video.dbclick = function(){
+    video.exitFullscreen();
+}
+video.dbclick = function(){
+    video.requestFullscreen();
+}
+let div = document.querySelectorAll('div');
+let header = document.querySelectorAll('header');
+let main = document.querySelectorAll('main');
+let p = document.querySelectorAll('p');
+let pre = document.querySelectorAll('pre');
+let h1 = document.querySelectorAll('h1');
+let img = document.querySelectorAll('img');
+let button = document.querySelectorAll('button');
+let li = document.querySelectorAll('li');
+let ul = document.querySelectorAll('ul');
+let iframe = document.querySelectorAll('iframe');
+let nav = document.querySelectorAll('nav');
+for(let i = 0;i < div.length;i++){
+    div[i].style.transition = '1s';
+    console.log(div[i]);
+}
+for(let i = 0;i < button.length;i++){
+    button[i].style.transition = '1s';
+    console.log(button[i]);
+}
+for(let i = 0;i < li.length;i++){
+    li[i].style.transition = '1s';
+    console.log(li[i]);
+}
+for(let i = 0;i < ul.length;i++){
+    ul[i].style.transition = '1s';
+    console.log(ul[i]);
+}
+for(let i = 0;i < iframe.length;i++){
+    iframe[i].style.transition = '1s';
+    console.log(iframe[i]);
+}
+for(let i = 0;i < nav.length;i++){
+    nav[i].style.transition = '1s';
+    console.log(nav[i]);
+}
+for(let i = 0;i < header.length;i++){
+    header[i].style.transition = '1s';
+    console.log(header[i]);
+}
+for(let i = 0;i < main.length;i++){
+    main[i].style.transition = '1s';
+    console.log(main[i]);
+}
+for(let i = 0;i < p.length;i++){
+    p[i].style.transition = '1s';
+    console.log(p[i]);
+}
+for(let i = 0;i < pre.length;i++){
+    pre[i].style.transition = '1s';
+    console.log(pre[i]);
+}
+for(let i = 0;i < h1.length;i++){
+    h1[i].style.transition = '1s';
+    console.log(h1[i]);
+}
+for(let i = 0;i < img.length;i++){
+    img[i].style.transition = '1s';
+    console.log(img[i]);
+}
+
